c93f2da0d579616888564e07039d6eb48e6e5f14
Haowei Hsu <hwhsu1231@localizethedocs.org>
2026-07-28 16:29:03 +0800

    ♻️ refactor(script): use patch logic for injecting js plugins
    Replace manual template file copying with the existing patch logic, so
    the template that injects `cmake/plugins` JavaScript into each generated
    page is handled automatically.
    
    Synced from: localizethedocs/cmake-docs-l10n@8c234b752156ef05ca3eed417fd708cfa0cc7ce6

--- cmake/targets/sphinx_update_pot.cmake
+++ cmake/targets/sphinx_update_pot.cmake
@@ -128,19 +128,6 @@ message("")
 restore_cmake_message_indent()
 
 
-message(STATUS "Copying 'page.html' files to the sphinx templates directory...")
-file(MAKE_DIRECTORY "${PROJ_OUT_REPO_DOCS_TMPLS_DIR}")
-file(COPY_FILE
-    "${PROJ_CMAKE_CUSTOM_DIR}/page.html"
-    "${PROJ_OUT_REPO_DOCS_TMPLS_DIR}/page.html")
-remove_cmake_message_indent()
-message("")
-message("From:  ${PROJ_CMAKE_CUSTOM_DIR}/page.html")
-message("To:    ${PROJ_OUT_REPO_DOCS_TMPLS_DIR}/page.html")
-message("")
-restore_cmake_message_indent()
-
-
 if (NOT UPDATE_POT_REQUIRED)
     message(STATUS "No need to update .pot files.")
     return()
