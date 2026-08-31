29a507975e59e29fbe6a10a95d607e32261e6eed
ltdrobot <ltdrobot@localizethedocs.org>
2026-08-31 17:22:24 +0000

    Deploying to pages from @ localizethedocs/syncthing-docs-l10n@0b0f737d22c244ed04376cb714d6791252d31b00 🚀

---
+++ ltd-config.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",     "English"],
+    ["sr-rs",     "Српски"],
+    ["zh-cn",     "简体中文"],
+    ["zh-tw",     "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["latest",    "Development"],
+    ["stable",    "Stable Release"],
+    ["2",         "Release 2.x"],
+    ["1",         "Release 1.x"],
+    ["0",         "Release 0.x"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",     "https://projects.localizethedocs.org"],
+    ["Crowdin",   "https://localizethedocs.crowdin.com/syncthing-docs-l10n"],
+    ["GitHub",    "https://github.com/localizethedocs/syncthing-docs-l10n"],
+    ["AtomGit",   "https://atomgit.com/localizethedocs/syncthing-docs-l10n"],
+    ["GitFlic",   "https://gitflic.ru/project/localizethedocs/syncthing-docs-l10n"],
+  ]
+};
